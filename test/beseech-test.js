fb3f1b0964444eff16a9f675e844858c19a45c93
indexzero <charlie.robbins@gmail.com>
2011-05-20 04:06:28 -0400

    [api test] Small updates to the beseech API. Update tests accordingly and add additonal test coverage

--- test/beseech-test.js
+++ test/beseech-test.js
@@ -13,7 +13,11 @@ var assert = require('assert'),
 vows.describe('beseech').addBatch({
   "When using beseech": {
     topic: function () {
-      beseech.start(helpers.stdin, helpers.stdout);
+      beseech.start({
+        stdin: helpers.stdin, 
+        stdout: helpers.stdout
+      });
+      
       return null;
     },
     "the readLine() method": {
@@ -48,7 +52,7 @@ vows.describe('beseech').addBatch({
           beseech.getInput('test input', this.callback);
           helpers.stdin.write('test value\n');
         },
-        "should write the specified data to stdout and respond with data": function (err, input) {
+        "should prompt to stdout and respond with data": function (err, input) {
           assert.isNull(err);
           assert.equal(input, 'test value');
           assert.isTrue(this.msg.indexOf('test input') !== -1);
@@ -65,42 +69,98 @@ vows.describe('beseech').addBatch({
             beseech.getInput(helpers.properties.username, this.callback);
             helpers.stdin.write('some-user\n');
           },
-          "should write the specified data to stdout and respond with data": function (err, input) {
+          "should prompt to stdout and respond with data": function (err, input) {
             assert.isNull(err);
             assert.equal(input, 'some-user');
             assert.isTrue(this.msg.indexOf('username') !== -1);
           }
-        }
-      },
-      "and an invalid input": {
-        topic: function () {
-          var that = this;
-          helpers.stdout.once('data', function (msg) {
-            that.msg = msg;
-          });
-
-          helpers.stderr.once('data', function (msg) {
-            that.errmsg = msg;
-          })
-
-          beseech.getInput(helpers.properties.username, this.callback);
-
-          beseech.once('invalid', function () {
-            beseech.once('prompt', function () {
-              process.nextTick(function () {
-                helpers.stdin.write('some-user\n');
-              })
-            })
-          });
-
-          helpers.stdin.write('some -user\n');
         },
-        "should prompt with an error before completing the operation": function (err, input) {
-          assert.isNull(err);
-          assert.equal(input, 'some-user');
-          assert.isTrue(this.errmsg.indexOf('Invalid input') !== -1);
-          assert.isTrue(this.msg.indexOf('username') !== -1);
+        "and an invalid input": {
+          topic: function () {
+            var that = this;
+            helpers.stdout.once('data', function (msg) {
+              that.msg = msg;
+            });
+
+            helpers.stderr.once('data', function (msg) {
+              that.errmsg = msg;
+            })
+
+            beseech.getInput(helpers.properties.username, this.callback);
+
+            beseech.once('invalid', function () {
+              beseech.once('prompt', function () {
+                process.nextTick(function () {
+                  helpers.stdin.write('some-user\n');
+                })
+              })
+            });
+
+            helpers.stdin.write('some -user\n');
+          },
+          "should prompt with an error before completing the operation": function (err, input) {
+            assert.isNull(err);
+            assert.equal(input, 'some-user');
+            assert.isTrue(this.errmsg.indexOf('Invalid input') !== -1);
+            assert.isTrue(this.msg.indexOf('username') !== -1);
+          }
         }
+      }
+    },
+    "the get() method": {
+      "with a simple string prompt": {
+        "that is not a property in beseech.properties": {
+          topic: function () {
+            var that = this;
+            helpers.stdout.once('data', function (msg) {
+              that.msg = msg;
+            })
+
+            beseech.get('test input', this.callback);
+            helpers.stdin.write('test value\n');
+          },
+          "should prompt to stdout and respond with the value": function (err, result) {
+            assert.isNull(err);
+            assert.include(result, 'test input');
+            assert.equal(result['test input'], 'test value');
+            assert.isTrue(this.msg.indexOf('test input') !== -1);
+          }
+        },
+        "that is a property name in beseech.properties": {
+          "with a default value": {
+            topic: function () {
+              var that = this;
+              
+              helpers.stdout.once('data', function (msg) {
+                that.msg = msg;
+              });
+              
+              beseech.properties['riffwabbles'] = helpers.properties['riffwabbles'];
+              beseech.get('riffwabbles', this.callback);
+              helpers.stdin.write('\n');
+            },
+            "should prompt to stdout and respond with the default value": function (err, result) {
+              assert.isNull(err);
+              assert.isTrue(this.msg.indexOf('riffwabbles') !== -1);
+              assert.isTrue(this.msg.indexOf('(foobizzles)') !== -1);
+              assert.include(result, 'riffwabbles');
+              assert.equal(result['riffwabbles'], helpers.properties['riffwabbles'].default);
+            }
+          }
+        }
+      }
+    },
+    "the addProperties() method": {
+      topic: function () {
+        beseech.addProperties({}, ['foo', 'bar'], this.callback);
+        helpers.stdin.write('foo\n');
+        helpers.stdin.write('bar\n');
+      },
+      "should add the properties to the object": function (err, obj) {
+        assert.isNull(err);
+        assert.isObject(obj);
+        assert.equal(obj.foo, 'foo');
+        assert.equal(obj.bar, 'bar');
       }
     }
   }
--- test/helpers.js
+++ test/helpers.js
@@ -44,29 +44,11 @@ util.error = function () {
 }
 
 helpers.properties = {
-  email: {
-    name: 'email',
-    validator: /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i,
-    warning: 'Must be a valid email address'
-  },
-  password: {
-    name: 'password',
-    hidden: true
-  },
-  inviteCode: {
-    name: 'invite code',
-    message: 'Invite Code',
-    validator: /^\w+$/,
-    warning: 'Invite code can only be letters, and numbers'
-  },
-  snapshot: {
-    name: 'snapshot',
-    message: 'Snapshot Name',
-    validator: /^[\w|\-|\.]+$/,
-    warning: 'Snapshot can only be letters, numbers, dashes, and dots',
-    default: (function () {
-      return '';
-    })()
+  riffwabbles: {
+    name: 'riffwabbles',
+    validator: /^[\w|\-]+$/,
+    warning: 'riffwabbles can only be letters, numbers, and dashes',
+    default: 'foobizzles'
   },
   username: {
     name: 'username',
