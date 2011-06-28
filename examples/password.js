31d0da9f633e1baea927709c2b28819928af6145
Bradley Meck <bradley.meck@gmail.com>
2011-06-27 19:59:22 -0500

    [fix] #5 added support for backspace, DEL, and standard escapes for HOME, END, ARROW LEFT / RIGHT

---
+++ examples/password.js
@@ -0,0 +1,25 @@
+/*
+ * simple-prompt.js: Simple example of using node-prompt.
+ *
+ * (C) 2010, Nodejitsu Inc.
+ *
+ */
+
+var prompt = require('../lib/prompt');
+
+//
+// Start the prompt
+//
+prompt.start();
+
+//
+// Get two properties from the user: username and password
+//
+prompt.get(['username', { name:'password', hidden: true}], function (err, result) {
+  //
+  // Log the results.
+  //
+  console.log('Command-line input received:');
+  console.log('  username: ' + result.username);
+  console.log('  password: ' + result.password);
+});
