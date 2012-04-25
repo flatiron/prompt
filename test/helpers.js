503527a224082c30c88dc15481c98cc044141522
Joshua Holbrook <josh.holbrook@gmail.com>
2012-04-25 00:40:06 -0700

    [test fix] Careless search/replace renamed some vars to have nonsense names.

--- test/helpers.js
+++ test/helpers.js
@@ -1,5 +1,5 @@
 /*
- * helpers.js: conform helpers for the prompt conforms.
+ * helpers.js: test helpers for the prompt tests.
  *
  * (C) 2010, Nodejitsu Inc.
  *
@@ -12,33 +12,33 @@ var events = require('events'),
 
 var helpers = exports;
 
-var MockReadWriconformream = helpers.MockReadWriconformream = function () {
+var MockReadWriteStream = helpers.MockReadWriteStream = function () {
   //
   // No need to do anything here, it's just a mock.
   //
 };
 
-util.inherits(MockReadWriconformream, events.EventEmitter);
+util.inherits(MockReadWriteStream, events.EventEmitter);
 
 ['resume', 'pause', 'setEncoding', 'flush'].forEach(function (method) {
-  MockReadWriconformream.prototype[method] = function () { /* Mock */ };
+  MockReadWriteStream.prototype[method] = function () { /* Mock */ };
 });
 
-MockReadWriconformream.prototype.write = function (msg) {
+MockReadWriteStream.prototype.write = function (msg) {
   this.emit('data', msg);
 };
 
 //
 // Create some mock streams for asserting against
-// in our prompt conforms.
+// in our prompt teSts.
 //
-helpers.stdin = new MockReadWriconformream();
-helpers.stdout = new MockReadWriconformream();
-helpers.stderr = new MockReadWriconformream();
+helpers.stdin = new MockReadWriteStream();
+helpers.stdout = new MockReadWriteStream();
+helpers.stderr = new MockReadWriteStream();
 
 //
 // Monkey punch `util.error` to silence console output
-// and redirect to helpers.stderr for conforming.
+// and redirect to helpers.stderr for testing.
 //
 util.error = function () {
   helpers.stderr.write.apply(helpers.stderr, arguments);
